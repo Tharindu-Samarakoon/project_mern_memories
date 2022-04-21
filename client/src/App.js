51ca72a0d7eb411c10ac593112c68be8a4f3db26
Tharindu-Samarakoon <tharusamara@gmail.com>
2022-04-21 13:30:13 +0530

    first

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
 import { useDispatch } from 'react-redux';
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Posts from './components/Posts/Posts';
 import Form from './components/Form/Form';
 import { getPosts } from './actions/posts';
 import useStyles from './styles';
 import memories from './images/memories.png';
+import Navbar from './components/Navbar/Navbar';
+import Auth from './components/Auth/Auth';
 
 const App = () => {
   const [currentId, setCurrentId] = useState(0);
@@ -18,11 +20,12 @@ const App = () => {
   }, [currentId, dispatch]);
 
   return (
+    <BrowserRouter>
     <Container maxWidth="lg">
-      <AppBar className={classes.appBar} position="static" color="inherit">
-        <Typography className={classes.heading} variant="h2" align="center">Memories</Typography>
-        <img className={classes.image} src={memories} alt="icon" height="60" />
-      </AppBar>
+      <Navbar />
+      <Routes>
+        <Route path='/Auth' exact component={Auth} />
+      </Routes>
       <Grow in>
         <Container>
           <Grid container justify="space-between" alignItems="stretch" spacing={3}>
@@ -35,7 +38,7 @@ const App = () => {
           </Grid>
         </Container>
       </Grow>
-    </Container>
+    </Container></BrowserRouter>
   );
 };
 
--- server/index.js
+++ server/index.js
@@ -4,9 +4,12 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+import dotenv from 'dotenv'
+
 import postRoutes from './routes/posts.js';
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
@@ -14,7 +17,7 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://js_mastery:123123123@practice.jto9p.mongodb.net/test';
+const CONNECTION_URL = process.env.MONGO_URI;
 const PORT = process.env.PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
